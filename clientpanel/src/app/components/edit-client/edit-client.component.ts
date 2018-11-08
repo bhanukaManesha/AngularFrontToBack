4ce728ad2650f537cca6569ec5d2c2452e20add7
bhanukaManesha <bhanukamgamage@gmail.com>
2018-11-08 12:26:20 +0800

    added update client

--- clientpanel/src/app/components/edit-client/edit-client.component.ts
+++ clientpanel/src/app/components/edit-client/edit-client.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ClientService } from '../../services/client.service'
+import { Client } from '../../models/Client'
+
+import { Router, ActivatedRoute,Params } from '@angular/router'
+import { FlashMessagesService } from 'angular2-flash-messages'
+
 @Component({
   selector: 'app-edit-client',
   templateUrl: './edit-client.component.html',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
+  id:string
+  client:Client = {
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:'',
+    balance:0
+    }
+  disableBalanceOnEdit:boolean = true;
+  constructor(
+    private clientService:ClientService,
+    private router:Router,
+    private route:ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id']
+    this.clientService.getClient(this.id).subscribe(client => this.client = client)
+
   }
 
+  onSubmit({value,valid}:{value:Client,valid:boolean}){
+      if (!valid){
+        this.flashMessage.show('Please fillout the form correctly',{
+            cssClass:'alert-danger',timeout:4000})
+      }else{
+        value.id = this.id
+        this.clientService.updateClient(value)
+        this.flashMessage.show('Client Updated',{
+          cssClass:'alert-success',timeout:4000})
+        this.router.navigate(['/client/' + this.id])
+      }
+
+  }
 }
